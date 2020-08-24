930be7c2564dddad114c37ef8e63256af4297103
Mr Garrix RL <rlfernando7@gmail.com>
2020-08-24 18:21:55 -0500

    Initialize objects in Python project with constructors

--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,7 @@
 from uberCar import UberCar
 from account import Account
 if __name__ == "__main__":
-    mazda = UberCar()
-    mazda.license = "QQO738"
-    mazda.driver = "Fercho"
-    mazda.passenger = 3
-    print(vars(mazda))
+
+    uberDriver = Account("Fernando Lagunes","INE")
+    mazda = UberCar("JDU394",uberDriver)
+    print(vars(mazda.driver))
