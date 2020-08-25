605d85c6651993a0c78424f12f9aae7f08785dcb
Mr Garrix RL <rlfernando7@gmail.com>
2020-08-24 19:16:00 -0500

    Implement Inheritance in Java classes and use super classes

--- Java/UberCar.java
+++ Java/UberCar.java
@@ -6,11 +6,12 @@ public class UberCar {
     Account driver;
     Integer passenger;
 
+    //Class constructor
     public UberCar(String license, Account driver) {
         this.license = license;
         this.driver = driver;
     }
-
+    //Class functions
     public void printDataCar(){
         System.out.println("License: "+license+" Driver: "+driver.name);
     }
