ecdeb004067c1df5267b80afe0afb41731b14825
Mr Garrix RL <rlfernando7@gmail.com>
2020-08-25 20:50:50 -0500

    Adding Different accounts in JS using inheritance

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,4 @@
-var account  = new Account("Fernando Lagunes","INE","rlfernando7@gmail.com","12345")
+var account  = new User("Fernando Lagunes","INE","rlfernando7@gmail.com","12345")
 var mazda = new UberCar("RDJ834",account,4)
 mazda.printDataCar();
 var jetta = new UberX("ALQ719",account,"Volkswagen","2003")
